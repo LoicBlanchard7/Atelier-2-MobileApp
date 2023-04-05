8cbdad8abd3baca2506781370633e8f0d4175b7b
Jarosz Léa <jarosz1u@etu.univ-lorraine.fr>
2023-04-05 15:20:25 +0200

    Changement lien pour l'invitation

--- reunionou/lib/screens/details.dart
+++ reunionou/lib/screens/details.dart
@@ -33,6 +33,8 @@ class _DetailspAppState extends State<DetailsApp> {
         return 'Absent(e)';
       case 'accepted':
         return 'Présent(e)';
+      case 'pending':
+        return 'En attente';
       default:
         return 'Invité(e)';
     }
@@ -134,7 +136,8 @@ class _DetailspAppState extends State<DetailsApp> {
           const SizedBox(height: 10),
           FloatingActionButton(
             onPressed: () async {
-              String link = "https://wawTropBienCetEvent.fr";
+              String link =
+                  "https://webetu.iutnc.univ-lorraine.fr/www/leblanc71u/atelier2/partage/event/${event.eid}";
               final share = await showDialog<bool>(
                 context: context,
                 builder: (context) {
