d2ba1951d1b9db291e8b532c3509538f17b0622e
LoicBlanchard7 <90914694+LoicBlanchard7@users.noreply.github.com>
2023-04-06 22:37:52 +0200

    Fix bug à cause de la correction

--- reunionou/lib/main.dart
+++ reunionou/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:reunionou/proviers/event_provider.dart';
-import 'package:reunionou/proviers/weather_provider.dart';
+import 'package:reunionou/providers/event_provider.dart';
+import 'package:reunionou/providers/weather_provider.dart';
 import 'package:reunionou/screens/signin.dart';
 
 void main() async {
